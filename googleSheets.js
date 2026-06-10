b4eb1bea85d0222cc3ddcedf3452c7d472753d73
theHARI962n <rajul1874@gmail.com>
2026-06-10 13:56:27 +0530

    added sheets and gitignore

---
+++ googleSheets.js
@@ -0,0 +1,36 @@
+const { google } = require("googleapis");
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: "credentials.json",
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+async function appendWaitlistRow(data) {
+  const client = await auth.getClient();
+
+  const sheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: "Sheet1!A:E",
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [
+        [
+          new Date().toLocaleString(),
+          data.name,
+          data.phone,
+          data.city,
+          data.course,
+        ],
+      ],
+    },
+  });
+}
+
+module.exports = {
+  appendWaitlistRow,
+};
